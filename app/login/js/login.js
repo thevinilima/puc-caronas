0dec75e5b00884391c3f24e889ec93b6ecf167a8
Vinicius Lima <thelimavinicius@gmail.com>
2022-06-27 10:28:38 -0300

    Login

--- app/login/js/login.js
+++ app/login/js/login.js
@@ -1,8 +1,24 @@
 const formEl = document.querySelector('form');
-const emailInput = document.getElementById('email');
-const passInput = document.getElementById('pass');
 
-const login = () => {};
+const login = () => {
+  const login = $('#login').val();
+  const pass = $('#pass').val();
+
+  if (!login || !pass) return;
+
+  const users = JSON.parse(localStorage.getItem('db_users'));
+  if (users) {
+    const user = users.find(
+      u => (u.email === login || u.code === login) && u.pass === pass
+    );
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+      location.href = '../home/';
+      return;
+    }
+  }
+  $('#login-error-msg').removeClass('hidden');
+};
 
 formEl.addEventListener('submit', e => {
   e.preventDefault();
